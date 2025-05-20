4988f990ec1d2f8f094b6970b35e761a63559f21
Sss <surkun1992@gmail.com>
2025-05-20 14:21:35 +0700

    добавление vuetify

---
+++ ProductionChain/frontend/src/plugins/vuetify.js
@@ -0,0 +1,12 @@
+﻿import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.min.css' 
+
+export default createVuetify({
+    components,
+    directives,
+    theme: {
+        defaultTheme: 'dark',
+    },
+})
--- ProductionChain/frontend/src/views/AboutView.vue
+++ ProductionChain/frontend/src/views/AboutView.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div class="about">
+        <h1>This is an about page</h1>
+        <template>
+        </template>
+            <v-btn color="primary">Кнопка Vuetify 3</v-btn>
+    </div>
 </template>
