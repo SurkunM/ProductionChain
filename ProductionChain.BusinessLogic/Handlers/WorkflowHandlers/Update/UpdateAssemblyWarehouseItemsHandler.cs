0260a5ddec5b6385eb2342696a96a5e76f0c117b
Sss <surkun1992@gmail.com>
2025-08-15 20:14:39 +0700

    добавдение Update hendlers

---
+++ ProductionChain.BusinessLogic/Handlers/WorkflowHandlers/Update/UpdateAssemblyWarehouseItemsHandler.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Logging;
+using ProductionChain.Contracts.Dto.Responses;
+using ProductionChain.Contracts.IRepositories;
+using ProductionChain.Contracts.IUnitOfWork;
+
+namespace ProductionChain.BusinessLogic.Handlers.WorkflowHandlers.Update;
+
+public class UpdateAssemblyWarehouseItemsHandler
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    private readonly ILogger<UpdateAssemblyWarehouseItemsHandler> _logger;
+
+    public UpdateAssemblyWarehouseItemsHandler(IUnitOfWork unitOfWork, ILogger<UpdateAssemblyWarehouseItemsHandler> logger)
+    {
+        _unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    }
+
+    public async Task HandleAsync(AssemblyWarehouseItemResponse projectDto)
+    {
+        var employeesRepository = _unitOfWork.GetRepository<IEmployeesRepository>();
+
+        employeesRepository.Update(requestDto.ToModel());
+
+        await _unitOfWork.SaveAsync();
+    }
+}
---
+++ ProductionChain.BusinessLogic/Handlers/WorkflowHandlers/Update/UpdateComponentsWarehouseItemsHandler.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.Logging;
+using ProductionChain.Contracts.Dto.Responses;
+using ProductionChain.Contracts.IRepositories;
+using ProductionChain.Contracts.IUnitOfWork;
+
+namespace ProductionChain.BusinessLogic.Handlers.WorkflowHandlers.Update;
+
+public class UpdateComponentsWarehouseItemsHandler
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    private readonly ILogger<UpdateComponentsWarehouseItemsHandler> _logger;
+
+    public UpdateComponentsWarehouseItemsHandler(IUnitOfWork unitOfWork, ILogger<UpdateComponentsWarehouseItemsHandler> logger)
+    {
+        _unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    }
+
+    public async Task<bool> HandleAsync(ComponentsWarehouseResponse componentsWarehouseDto)
+    {
+        var projectsRepository = _unitOfWork.GetRepository<IProductsRepository>();
+        var employeeRepository = _unitOfWork.GetRepository<IEmployeesRepository>();
+
+        try
+        {
+            _unitOfWork.BeginTransaction();
+
+            var manager = await employeeRepository.GetEmployeesAsync(componentsWarehouseDto.Id);
+
+            if (manager is null)
+            {
+                _unitOfWork.RollbackTransaction();
+
+                return false;
+            }
+
+            projectsRepository.Update(projectDto.ToModel(manager));
+
+            await _unitOfWork.SaveAsync();
+
+            return true;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Failed to create project. Transaction rolled back");
+
+            _unitOfWork.RollbackTransaction();
+
+            throw;
+        }
+    }
+}
