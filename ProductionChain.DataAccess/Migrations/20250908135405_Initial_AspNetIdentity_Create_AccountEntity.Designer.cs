6267705f8003490791a5e8a80de13b738cadb7f0
Sss <surkun1992@gmail.com>
2025-09-08 21:12:22 +0700

    Миграция в Identity 2.0, Create Entity "Account"

--- ProductionChain.DataAccess/Migrations/20250908135405_Initial_AspNetIdentity_Create_AccountEntity.Designer.cs
+++ ProductionChain.DataAccess/Migrations/20250908135405_Initial_AspNetIdentity_Create_AccountEntity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProductionChain.DataAccess;
 
@@ -11,9 +12,11 @@ using ProductionChain.DataAccess;
 namespace ProductionChain.DataAccess.Migrations
 {
     [DbContext(typeof(ProductionChainDbContext))]
-    partial class ProductionChainDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250908135405_Initial_AspNetIdentity_Create_AccountEntity")]
+    partial class Initial_AspNetIdentity_Create_AccountEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
